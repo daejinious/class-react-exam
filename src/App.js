857d415acecbe234c9e1d3eb3c62b792c2456deb
daejinious <size_chi@naver.com>
2021-05-13 16:52:01 +0900

    react-router-dom을 이용한 routing 작업

--- src/App.js
+++ src/App.js
@@ -1,56 +1,32 @@
 import React, {useEffect, useState} from "react";
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+import Rooms from "./Rooms";
 
 function App() {
-  const [pageName, setPageName] = useState('')
-
-
-  useEffect(() => {
-    window.onpopstate = (event) => {
-      console.log(`location: ${document.location}, state: ${event.state}`);
-      const { state } = event
-
-      setPageName(state)
-    }
-  }, []);
-
-  const onClick1 = () => {
-    const pageName = 'page1'
-    window.history.pushState(pageName, '', '/page1')
-    setPageName(pageName)
-  }
-
-  const onClick2 = () => {
-    const pageName = 'page2'
-    window.history.pushState(pageName, '', '/page2')
-    setPageName(pageName)
-  }
-
   return (
-    <div>
-      <button onClick={onClick1}>
-        page1
-      </button>
-      <button onClick={onClick2}>
-        page2
-      </button>
+    <BrowserRouter>
+      <div style={{ padding: 20, border: '5px solid gray' }}>
+        <Link to="/">홈</Link><br />
+        <Link to="/photo">사진</Link><br/>
+        <Link to="/rooms">방 소개</Link><br/>
 
-      {!pageName && <Home />}
-      {pageName === 'page1' && <Page1 />}
-      {pageName === 'page2' && <Page2 />}
-    </div>
-  );
+        <Route exact path="/" component={Home} />
+        <Route path="/photo" component={Photo} />
+        <Route path="/rooms" component={Rooms} />
+      </div>
+
+
+    </BrowserRouter>
+  )
+
 }
 
 function Home() {
   return <h2>여기는 홈페이지 입니다.</h2>
 }
 
-function Page1() {
-  return <h2>여기는 page 1 입니다.</h2>
-}
-
-function Page2() {
-  return <h2>여기는 page 2 입니다.</h2>
+function Photo() {
+  return <h2>여기는 Photo 입니다.</h2>
 }
 
 export default App;
