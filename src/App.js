4845509870e112a33e8b8f075bc23913443db7f6
daejinious <size_chi@naver.com>
2021-05-13 18:43:27 +0900

    react useState를 이용한 Component 속성값과 상태값 제어

--- src/App.js
+++ src/App.js
@@ -1,50 +1,25 @@
 import React, { useState } from 'react'
+import Title from './Title'
 
 function App() {
-  const [todoList, setTodoList] = useState([])
-  const [currentId, setCurrentId] = useState(1)
-  const [desc, setDesc] = useState('')
-  const [showOdd, setShowOdd] = useState(false)
+  const [color, setColor] = useState('red')
+  const [title, setTitle] = useState('red')
+  const [state, setState] = useState(false)
 
-  const onAdd = () => {
-    const todo = { id: currentId, desc }
-    setCurrentId(currentId + 1)
-    setTodoList([...todoList, todo])
-    setDesc('')
+  const setColorClick = () => {
+    let currentColor = 'red'
+    if (!state) {
+      currentColor = 'blue'
+    }
+    setState(!state)
+    setColor(currentColor)
+    setTitle(currentColor)
   }
-
-  const onDelete = (e) => {
-    const id = Number(e.target.dataset.id)
-    const newTodoList = todoList.filter((todo) => todo.id !== id)
-    setTodoList(newTodoList)
-  }
-
-  const onSaveToServer = () => {}
 
   return (
-    <div>
-      <h3>할 일 목록</h3>
-      <ul>
-        {todoList
-          .filter((_, index) => (showOdd ? index % 2 === 0 : true))
-          .map((todo) => (
-            <li key={todo.id}>
-              <span>{todo.desc}</span>
-              <button data-id={todo.id} onClick={onDelete}>
-                삭제
-              </button>
-            </li>
-          ))}
-      </ul>
-      <input
-        type="text"
-        value={desc}
-        onChange={(e) => setDesc(e.target.value)}
-      />
-      <button onClick={onAdd}>추가</button>
-      <button onClick={() => setShowOdd(!showOdd)}>홀수</button>
-      <button onClick={onSaveToServer}>서버에 저장</button>
-    </div>
+    <button style={{ backgroundColor: color }} onClick={setColorClick}>
+      <Title title={title} />
+    </button>
   )
 }
 
