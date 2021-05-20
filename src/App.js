e624fef5d1b813995a87c7bcb8af117d2e7a57b4
daejinious <size_chi@naver.com>
2021-05-20 17:03:10 +0900

    reducer 이용하기

--- src/App.js
+++ src/App.js
@@ -1,29 +1,44 @@
 import React from 'react'
-import { applyMiddleware, createStore } from 'redux'
+import produce from 'immer'
 
-const delayAction = (store) => (next) => (action) => {
-  const delay = action.meta?.delay // action.meta && action.meta.delay
-  if (!delay) {
-    return next(action)
-  }
-
-  const timeoutId = setTimeout(() => next(action), delay)
-  return function cancel() {
-    clearTimeout(timeoutId)
-  }
+const prevState = {
+  user: {
+    name: 'mike',
+    friends: [
+      {
+        name: 'jane',
+        age: 23,
+      },
+      {
+        name: 'jake',
+        age: 24,
+      },
+    ],
+  },
+  products: [],
 }
 
-const myReducer = (state = { name: 'mike' }, action) => {
-  console.log('myReducer')
-  if (action.type === 'someAction') {
-    return { name: 'mike2' }
-  }
-  return state
-}
+const nextState = produce(prevState, (draft) => {
+  draft.user.friends[0].age = 33
+})
+console.log('prevState === nextState', prevState === nextState)
+console.log(
+  'prevState.user.friends[0] === nextState.user.friends[0]',
+  prevState.user.friends[0] === nextState.user.friends[0]
+)
 
-const store = createStore(myReducer, applyMiddleware(delayAction))
-store.dispatch({ type: 'someAction', meta: { delay: 3000 } })
+console.log(
+  'prevState.user.friends[1] === nextState.user.friends[1]',
+  prevState.user.friends[1] === nextState.user.friends[1]
+)
 
-export default function App() {
+console.log(
+  'prevState.products === nextState.products',
+  prevState.products === nextState.products
+)
+
+function App() {
   return <div>실전 리액트</div>
 }
+
+export default App
