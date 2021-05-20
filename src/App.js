0056611f8f2a387747f22ff55cf7a413e1c44073
daejinious <size_chi@naver.com>
2021-05-20 10:52:56 +0900

    Component prop이 변경되어야 할 때 안되는 문제 해결 방법

--- src/App.js
+++ src/App.js
@@ -4,20 +4,38 @@ export default function App() {
   return (
     <div>
       실적 리액트
-      <Profile />
+      <SelectFruit />
     </div>
   )
 }
 
-function Profile({ userId }) {
-  const [user, setUser] = useState()
-  const [needDetail, setNeedDetail] = useState(false)
+function SelectFruit() {
+  const [fruits, setFruits] = useState(['apple', 'banana', 'orange'])
+  const [newFruit, setNewFruit] = useState([''])
 
-  useEffect(() => {
-    fetchUser(userId, needDetail).then((data) => setUser(data))
-  }, [userId, needDetail])
-  console.log(user, setNeedDetail)
-  return null
+  function addNewFruit() {
+    // fruits.push(newFruit)
+    setFruits([...fruits, newFruit])
+    setNewFruit('')
+  }
+
+  return (
+    <div>
+      <Select options={fruits} />
+      <input
+        type="text"
+        value={newFruit}
+        onChange={(e) => setNewFruit(e.target.value)}
+      />
+      <button onClick={addNewFruit}>추가하기</button>
+    </div>
+  )
 }
 
-function fetchUser(userId, needDetail) {}
+const Select = React.memo(({ options }) => (
+  <div>
+    {options.map((item, index) => (
+      <p key={index}>{item}</p>
+    ))}
+  </div>
+))
