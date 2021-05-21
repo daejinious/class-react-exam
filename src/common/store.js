55526261fe1d0118fdcfaace402aae74d465fac1
daejinious <size_chi@naver.com>
2021-05-21 14:55:16 +0900

    redux saga를 이용항 비동기 액션 처리

--- src/common/store.js
+++ src/common/store.js
@@ -1,11 +1,26 @@
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux'
 import timelineReducer from '../timeline/state'
 import friendReducer from '../friend/state'
+import createSagaMiddleware from 'redux-saga'
+import { all } from 'redux-saga/effects'
+import timelineSaga from '../timeline/state/saga'
 
 const reducer = combineReducers({
   timeline: timelineReducer,
   friend: friendReducer,
 })
 
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__?.())
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const sagaMiddleware = createSagaMiddleware()
+const store = createStore(
+  reducer,
+  composeEnhancers(applyMiddleware(sagaMiddleware))
+)
+
+function* rootSaga() {
+  yield all([timelineSaga()])
+}
+
+sagaMiddleware.run(rootSaga)
+
 export default store
--- src/timeline/container/TimelineMain.js
+++ src/timeline/container/TimelineMain.js
@@ -1,22 +1,30 @@
 import React from 'react'
 import { getNextTimeline } from '../../common/mockData'
-import { addTimeline } from '../state'
+import { actions } from '../state'
 import TimelineList from '../component/TimelineList'
 import { useDispatch, useSelector } from 'react-redux'
 
 function TimelineMain() {
-  const timelines = useSelector((state) => state.timeline.timelines)
   const dispatch = useDispatch()
+  const timelines = useSelector((state) => state.timeline.timelines)
+  const isLoading = useSelector((state) => state.timeline.isLoading)
 
   function onAdd() {
     const timeline = getNextTimeline()
-    dispatch(addTimeline(timeline))
+    dispatch(actions.addTimeline(timeline))
+  }
+
+  function onLike(e) {
+    const id = Number(e.target.dataset.id)
+    const timeline = timelines.find((item) => item.id === id)
+    dispatch(actions.requestLike(timeline))
   }
 
   return (
     <div>
       <button onClick={onAdd}>타임라인 추가</button>
-      <TimelineList timelines={timelines} />
+      <TimelineList timelines={timelines} onLike={onLike} />
+      {isLoading && <p>전송 중...</p>}
     </div>
   )
 }
